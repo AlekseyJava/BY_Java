7244f00e5af27bc69e131c93ec21367143b6a639
Aleksey Zvarich <zvaraleksey@gmail.com>
2019-12-14 09:46:06 +0400

    Repair Decomposition/16_Task

--- 2_Algorithmization/Decomposition/16_Task/src/sumOfOddNumber/FindNumbers.java
+++ 2_Algorithmization/Decomposition/16_Task/src/sumOfOddNumber/FindNumbers.java
@@ -2,9 +2,21 @@ package sumOfOddNumber;
 
 public class FindNumbers {
     public void numbers(int n){
+        boolean b = true;
         int array[] = new int[n];
         ArrayDigitsNumber arrayDigitsNumber = new ArrayDigitsNumber();
         for (int i = (int)Math.pow(10,n-1); i < (int)Math.pow(10,n); i++){
+            array = arrayDigitsNumber.arrayDigits(n, i);
+            for(int j = 0; j<n; j++){
+                if(array[j]%2==0){
+                    b = false;
+                }
+            }
+            if (b == true) {
+                System.out.println("Найдено число состоящее из нечетных цифр: " + i);
+                //b = false;
+            }
+            b = true;
 
         }
     }
