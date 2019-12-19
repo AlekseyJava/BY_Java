19ab618588d584d716cec83fff38bb595623f9ac
Aleksey Zvarich <zvaraleksey@gmail.com>
2019-12-19 22:00:14 +0400

    Added StringBuilder/01_Task

---
+++ 3_Strings_and_basics_of_text_processing/StringBuilder/01_Task/src/numbersSpaces/NumbersSpaces.java
@@ -0,0 +1,15 @@
+package numbersSpaces;
+
+public class NumbersSpaces {
+    public static void main(String[] args) {
+        String s = "My spaces   3    4     5 2";
+        int n = 0;
+        int maxSpeces = 0;
+        n = s.length();
+        System.out.println(n);
+
+        for(int i = 0; i < n; i++){
+            
+        }
+    }
+}
