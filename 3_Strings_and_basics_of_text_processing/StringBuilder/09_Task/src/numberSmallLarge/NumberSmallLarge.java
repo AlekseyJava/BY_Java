0f1cb1f2c9d165713eb3f0b64ce5c5dc79848065
Aleksey Zvarich <zvaraleksey@gmail.com>
2019-12-27 20:53:12 +0400

    Added StringBuilder/09_Task

---
+++ 3_Strings_and_basics_of_text_processing/StringBuilder/09_Task/src/numberSmallLarge/NumberSmallLarge.java
@@ -0,0 +1,25 @@
+package numberSmallLarge;
+//Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке.
+//Учитывать только английские
+//буквы.
+public class NumberSmallLarge {
+    public static void main(String[] args) {
+        String s = "Hello World! My name is Aleksey!";
+        StringBuilder s1 = new StringBuilder("Hello! It is a StringBulilderObject");
+        int countSmallLeters = 0;
+        int capitallLeters = 0;
+
+        for (int i = 0; i < s1.length(); i++){
+            //if (Character.toLowerCase(s.charAt(i))){
+            if (Character.isUpperCase(s1.charAt(i))){
+                System.out.println("Большая буква");
+                capitallLeters++;
+            }
+            else {
+                countSmallLeters++;
+            }
+        }
+        System.out.println(countSmallLeters);
+        System.out.println(capitallLeters);
+    }
+}
